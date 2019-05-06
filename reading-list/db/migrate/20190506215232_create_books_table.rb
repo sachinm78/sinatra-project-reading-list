bf061c80067728675d24cba4c39e3af517f88dbd
Sachinm78 <sachinm78@gmail.com>
2019-05-06 14:58:03 -0700

    created a new app with corneal gem and created initial migrations and tables

---
+++ reading-list/db/migrate/20190506215232_create_books_table.rb
@@ -0,0 +1,9 @@
+class CreateBooksTable < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.string :genre
+      t.integer :user_id
+  end
+end
