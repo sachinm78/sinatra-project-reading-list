9341dfda4cfd4b0cbea2cf3410560e2e1d400e17
Sachinm78 <sachinm78@gmail.com>
2019-05-07 11:42:38 -0700

    added routes for login and logout

--- reading-list/app/controllers/users_controller.rb
+++ reading-list/app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
         if !session[:user_id]
         erb :'users/new'
         else
-        redirect to '/signup'
+        redirect to '/books'
         end
     end
 
@@ -14,7 +14,35 @@ class UsersController < ApplicationController
         else
         @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
         session[:user_id] = @user.id
-        redirect '/books/new'
+        redirect to '/books'
         end
     end
+
+    get '/login' do 
+        @error_message = params[:error]
+        if !session[:user_id]
+          erb :'users/login'
+        else
+          redirect to '/books/index'
+        end
+      end
+    
+      post '/login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect to '/index'
+        else
+          redirect to '/signup'
+        end
+      end
+    
+      get '/logout' do
+        if session[:user_id] != nil
+          session.destroy
+          redirect to '/login'
+        else
+          redirect to '/'
+        end
+      end
 end
