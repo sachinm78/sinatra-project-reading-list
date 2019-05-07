4e7e753759799de21b3d9aea5a4cb210e9bf2b0f
Sachinm78 <sachinm78@gmail.com>
2019-05-07 09:19:47 -0700

    created user controller and code needed to view sign-up page

---
+++ reading-list/app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController 
+
+    get '/signup' do
+        if !session[:user_id]
+        erb :'users/new'
+        else
+        redirect to '/signup'
+        end
+    end
+
+    post '/signup' do 
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+        else
+        @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+        session[:user_id] = @user.id
+        redirect '/books'
+        end
+    end
+end
