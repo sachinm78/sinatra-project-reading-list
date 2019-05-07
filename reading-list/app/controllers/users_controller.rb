46fa62c1deba5d9bc80b9f53785f6e177e4d8d86
Sachinm78 <sachinm78@gmail.com>
2019-05-07 09:38:04 -0700

    created views for signup and login.

--- reading-list/app/controllers/users_controller.rb
+++ reading-list/app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController 
 
     get '/signup' do
-        if !session[:user_id]
-        erb :'users/new'
-        else
-        redirect to '/signup'
-        end
+        puts "test"
+        # if !session[:user_id]
+        # erb :'users/new'
+        # else
+        # redirect to '/signup'
+        # end
     end
 
-    post '/signup' do 
-        if params[:username] == "" || params[:email] == "" || params[:password] == ""
-        redirect to '/signup'
-        else
-        @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-        session[:user_id] = @user.id
-        redirect '/books'
-        end
-    end
+    # post '/signup' do 
+    #     if params[:username] == "" || params[:email] == "" || params[:password] == ""
+    #     redirect to '/signup'
+    #     else
+    #     @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+    #     session[:user_id] = @user.id
+    #     redirect '/books'
+    #     end
+    # end
 end
