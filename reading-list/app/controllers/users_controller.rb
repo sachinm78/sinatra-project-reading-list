9982640e680550f6bcb074f82c60c345cbd6a0b1
Sachinm78 <sachinm78@gmail.com>
2019-05-07 10:04:59 -0700

    mounted users_controller and tested signup page

--- reading-list/app/controllers/users_controller.rb
+++ reading-list/app/controllers/users_controller.rb
@@ -1,21 +1,20 @@
 class UsersController < ApplicationController 
 
     get '/signup' do
-        puts "test"
-        # if !session[:user_id]
-        # erb :'users/new'
-        # else
-        # redirect to '/signup'
-        # end
+        if !session[:user_id]
+        erb :'users/new'
+        else
+        redirect to '/signup'
+        end
     end
 
-    # post '/signup' do 
-    #     if params[:username] == "" || params[:email] == "" || params[:password] == ""
-    #     redirect to '/signup'
-    #     else
-    #     @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-    #     session[:user_id] = @user.id
-    #     redirect '/books'
-    #     end
-    # end
+    post '/signup' do 
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+        else
+        @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+        session[:user_id] = @user.id
+        redirect '/books'
+        end
+    end
 end
