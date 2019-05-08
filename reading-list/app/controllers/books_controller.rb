de18c3cc754672f0aedf86ae5717c5b3317e7180
Sachinm78 <sachinm78@gmail.com>
2019-05-08 11:27:41 -0700

    created patch route and delete route for books; added delete.erb

--- reading-list/app/controllers/books_controller.rb
+++ reading-list/app/controllers/books_controller.rb
@@ -36,4 +36,29 @@ class BooksController < ApplicationController
       @book.update(params.select{|i| i=="title" || i=="author" || i=="genre"})
       redirect "/books/#{@book.id}"
     end
+
+    get "/books/:id/edit" do
+      redirect_if_not_logged_in 
+      @error_message = params[:error]
+      @book = Book.find(params[:id])
+      erb :'books/edit'
+    end
+
+    patch '/books/:id' do
+      id = params[:id]
+      new_params = {}
+      old_book = Book.find(id)
+      new_params[:title] = params["title"]
+      new_params[:author] = params["author"]
+      new_params[:genre] = params["genre"]
+      old_book.update(new_params)
+  
+      redirect "/books/#{old_book.id}"
+    end
+  
+    delete '/books/:id/delete' do
+      @book = Book.find(params[:id])
+      @book.destroy
+      erb :delete
+    end
 end
