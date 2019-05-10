04dcbac3179ea6ae4ab10857cc3fb8fe74956018
Sachinm78 <sachinm78@gmail.com>
2019-05-10 06:57:50 -0700

    added validations to user.rb and book.rb models

--- reading-list/app/models/user.rb
+++ reading-list/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :books
+
+    validates :username, :email, :password, presence: true
+    validates :username, :email, uniqueness: true
 end
